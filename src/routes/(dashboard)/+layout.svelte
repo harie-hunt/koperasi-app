0f9412cfc4e195d3049631fa78450934dc13c5be
momo-hunt <momohunt8@gmail.com>
2024-08-14 19:24:22 +0700

    add-load

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -4,6 +4,9 @@
 	import { Button } from '$lib/ui/button';
 	import { Form } from '$lib/ui/form';
 	import { SideBrand, SideMenu } from '$lib/co-pages';
+	import { navigating, page } from '$app/stores';
+	import Icon from '@iconify/svelte';
+	import { fly } from 'svelte/transition';
 
 	export let data: LayoutData;
 	$: ({ user } = data);
@@ -25,8 +28,20 @@
 			</Form>
 		</header>
 
-		<main class="p-6">
-			<slot />
-		</main>
+		{#if $navigating}
+			<div class="absolute flex items-center gap-2 bg-white p-6">
+				<Icon icon="mdi:loading" class="h-6 w-6 animate-spin" />
+				Loading
+			</div>
+		{/if}
+		{#key $page.url.pathname}
+			<main
+				in:fly={{ y: 20, duration: 200, delay: 200 }}
+				out:fly={{ y: 20, duration: 200, delay: 0 }}
+				class="p-6"
+			>
+				<slot />
+			</main>
+		{/key}
 	</div>
 </div>
